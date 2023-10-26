8c3f6b67be7819a68e768a2c6b1855a395f74cb1
freakcao <freakcao@zego.im>
2023-10-26 12:05:43 +0800

    2

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -15,6 +15,12 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f github.com/fontcao/blogs.git dev:dev
+##git remote add origin github.com/fontcao/blogs.git
+#git push -f origin main:gh-pages
+#git branch dev
+#git checkout dev
+#git push -u origin main:dev -f
 
+#git push -f github.com/fontcao/blogs.git main:dev
+git push -f https://github.com/fontcao/blogs.git main:gh-pages
 cd -
